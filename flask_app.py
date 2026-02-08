658363ab6fef39e403627cc8745c260602ba3847
shaney hoyohoy <shakalili86@gmail.com>
2026-02-07 21:03:14 -1000

    Fix Render deployment: add gunicorn, fix port binding

--- flask_app.py
+++ flask_app.py
@@ -1,3 +1,4 @@
+import os
 from app import create_app
 from app.models import db
 
@@ -7,5 +8,7 @@ app = create_app('ProductionConfig')
 with app.app_context():
     db.create_all()
 
-app.run(host='127.0.0.1', port=5000, debug=True)
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
     
