4d92709b1040b525c725e1ddd053136b7aca184e
shaney hoyohoy <shakalili86@gmail.com>
2026-02-08 00:06:40 -1000

    refactor config and flask_app for consistency and clarity

--- config.py
+++ config.py
@@ -9,16 +9,11 @@ class DevelopmentConfig:
     CACHE_DEFAULT_TIMEOUT = 300
     
 class TestingConfig:
-        SQLALCHEMY_DATABASE_URI = 'sqlite:///:testing.db'
-        DEBUG = True
-        CACHE_TYPE= 'SimpleCache'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///:testing.db'
+    DEBUG = True
+    CACHE_TYPE= 'SimpleCache'
         
+
 class ProductionConfig:
-    uri = os.environ.get("DATABASE_URL")  # Render provides DATABASE_URL
-
-    # Fix Render's postgres:// → postgresql:// issue
-    if uri and uri.startswith("postgres://"):
-        uri = uri.replace("postgres://", "postgresql://", 1)
-
-    SQLALCHEMY_DATABASE_URI = uri
-    CACHE_TYPE = "SimpleCache"
+   SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
+   CACHE_TYPE = 'SimpleCache'
--- flask_app.py
+++ flask_app.py
@@ -7,5 +7,5 @@ app = create_app('ProductionConfig')
 with app.app_context():
     db.create_all()
 
-app.run(host='127.0.0.1', port=5000, debug=True)
+
     
